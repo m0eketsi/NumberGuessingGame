76d32ae0b3de3d9d48cc1c5ef55ae70925529bff
Moeketsi Moloi <moloimason@gmail.com>
2022-08-17 11:03:05 -0700

    update

--- Number Guessing Game/Assets/Scripts/Number.cs
+++ Number Guessing Game/Assets/Scripts/Number.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class Number : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private int guess;
+    private int minValue = 1;
+   private int maxValue = 10;
+
     void Start()
     {
-        
+        Debug.Log("Welcome to our Number Guessing Game.");
+        Debug.Log("The number range is from " + minValue + "and" + maxValue);
     }
 
-    // Update is called once per frame
     void Update()
     {
         
